6802bbdbbb61e082123649f016fc5468427a0680
PawelFornal <pawelfornal91@gmail.com>
2024-11-30 23:51:08 +0100

    fix

--- cypress/support/utils/scheduleTable.ts
+++ cypress/support/utils/scheduleTable.ts
@@ -13,25 +13,25 @@ export const scheduleTable = {
             cy.contains(tableSchedule.rowEmployeeName, employeeName)
                 .parents('tr')
                 .find(tableSchedule.fieldToChange)
-                .click({ force: true }); // jeśli daty są takie same, klika w znalezioną datę
+                .click({ force: true });
         } else {
             let targetColumnIndex: number;
             cy.get(tableSchedule.headerDay)
                 .each(($headerCell, index) => {
                     if ($headerCell.find('span').text().includes(dayToChange)) {
-                        targetColumnIndex = index; // znajduje indeks kolumny z datą do zmiany
-                        return false; // przerywa iterację po znalezionej kolumnie
+                        targetColumnIndex = index;
+                        return false;
                     }
                 })
                 .then(() => {
                     cy.get(tableSchedule.tableRow)
                         .each(($row) => {
-                            if ($row.find(tableSchedule.rowEmployeeName).text().includes(employeeName)) { // znajduje wiersz z pracownikiem
+                            if ($row.find(tableSchedule.rowEmployeeName).text().includes(employeeName)) {
                                 cy.wrap($row)
                                     .find(tableSchedule.cellSelector)
                                     .eq(targetColumnIndex)
-                                    .click({ force: true }); // klika w znalezioną komórkę na przecięciu wiersza i kolumny
-                                return false; // przerywa iterację po znalezionym wierszu
+                                    .click({ force: true });
+                                return false;
                             }
                         });
                 });
